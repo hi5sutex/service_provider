bf3b4c25aa38c45aaefb35bbb1c66a46ecd15e9a
abpatel070 <mrabpatel070@gmail.com>
2024-12-18 09:05:50 +0530

    Account page 3 abhi

--- lib/User Panel/user_account.dart
+++ lib/User Panel/user_account.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:service_provider/User Panel/user_booking.dart'; // Add your actual Booking Page
-import 'package:service_provider/User Panel/user_chat.dart';    // Add your actual Chat Page
-import 'package:service_provider/User Panel/user_home.dart';    // Add your actual Home Page
+import 'package:service_provider/User Panel/user_chat.dart'; // Add your actual Chat Page
+import 'package:service_provider/User Panel/user_home.dart'; // Add your actual Home Page
 
 class AccountPage extends StatefulWidget {
   @override
@@ -28,7 +28,7 @@ class _AccountPageState extends State<AccountPage> {
     if (user != null) {
       DocumentSnapshot userDoc = await FirebaseFirestore.instance
           .collection('users') // Make sure your collection name is 'users'
-          .doc(user.uid)        // Retrieve document using user's UID
+          .doc(user.uid) // Retrieve document using user's UID
           .get();
 
       setState(() {
@@ -64,7 +64,7 @@ class _AccountPageState extends State<AccountPage> {
         );
         break;
       case 3:
-      // Do nothing, already on Account Page
+        // Do nothing, already on Account Page
         break;
     }
   }
@@ -179,13 +179,23 @@ class AccountPageBody extends StatelessWidget {
 
           // List Options
           _ListMenuItem(icon: Icons.event, label: "My Plans", onTap: () {}),
-          _ListMenuItem(icon: Icons.wallet_giftcard, label: "Wallet", onTap: () {}),
-          _ListMenuItem(icon: Icons.card_membership, label: "Plus membership", onTap: () {}),
-          _ListMenuItem(icon: Icons.star_border, label: "My rating", onTap: () {}),
-          _ListMenuItem(icon: Icons.location_on, label: "Manage addresses", onTap: () {}),
-          _ListMenuItem(icon: Icons.payment, label: "Manage payment methods", onTap: () {}),
+          _ListMenuItem(
+              icon: Icons.wallet_giftcard, label: "Wallet", onTap: () {}),
+          _ListMenuItem(
+              icon: Icons.card_membership,
+              label: "Plus membership",
+              onTap: () {}),
+          _ListMenuItem(
+              icon: Icons.star_border, label: "My rating", onTap: () {}),
+          _ListMenuItem(
+              icon: Icons.location_on, label: "Manage addresses", onTap: () {}),
+          _ListMenuItem(
+              icon: Icons.payment,
+              label: "Manage payment methods",
+              onTap: () {}),
           _ListMenuItem(icon: Icons.settings, label: "Settings", onTap: () {}),
-          _ListMenuItem(icon: Icons.info_outline, label: "About UC", onTap: () {}),
+          _ListMenuItem(
+              icon: Icons.info_outline, label: "About UC", onTap: () {}),
         ],
       ),
     );
@@ -199,14 +209,42 @@ class _MenuItem extends StatelessWidget {
   const _MenuItem({required this.icon, required this.label, Key? key})
       : super(key: key);
 
+  void _handleTap(BuildContext context) {
+    switch (label) {
+      case "My bookings":
+        // Navigate to bookings page
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => BookingPage()),
+        );
+        break;
+      case "Native devices":
+        // You can add navigation or show a dialog for native devices
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Native Devices feature coming soon'),
+            duration: Duration(seconds: 1),
+          ),
+        );
+        break;
+      case "Help & support":
+        // Navigate to help and support page or show support options
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Help & Support feature coming soon'),
+            duration: Duration(seconds: 1),
+          ),
+        );
+        break;
+      default:
+        print('No action defined for $label');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () {
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text('$label feature coming soon')),
-        );
-      },
+      onTap: () => _handleTap(context),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
